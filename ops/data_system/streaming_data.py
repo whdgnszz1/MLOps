dce3ab94f7a1dc613d85565ecfb82432f20a2873
홍종훈 <whdgnszz1@snu.ac.kr>
2025-04-13 21:15:19 +0900

    feat: 빅쿼리에서의 스트리밍 데이터 처리

---
+++ ops/data_system/streaming_data.py
@@ -0,0 +1,48 @@
+from google.cloud import bigquery
+import uuid
+import time
+import random
+import string
+
+
+def generate_random_text(length: int = 10) -> str:
+    letters = string.ascii_letters + string.digits
+    return "".join(random.choice(letters) for i in range(length))
+
+
+client = bigquery.Client.from_service_account_json("../config/machine-learning-456412-ff653903a9e1.json")
+table_id = "machine-learning-456412.introduction.streaming"
+
+try:
+    client.get_table(table_id)
+    print("Table {} already exists.".format(table_id))
+except:
+    schema = [
+        bigquery.SchemaField(name="log_id", field_type="STRING"),
+        bigquery.SchemaField(name="text", field_type="STRING"),
+        bigquery.SchemaField(name="date", field_type="INTEGER"),
+    ]
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table)
+    print(
+        "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+    )
+
+
+def insert_new_line() -> None:
+    rows_to_insert = [
+        {
+            "log_id": str(uuid.uuid4()),
+            "text": generate_random_text(50),
+            "date": int(time.time()),
+        },
+    ]
+
+    errors = client.insert_rows_json(table_id, rows_to_insert)
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
+
+insert_new_line()
